7489171753252c46330889a22e5ec1b0196be54c
shamil-th <shamil.stackup@gmail.com>
2024-01-27 18:26:21 +0530

    test

--- server/controller/applicationController.js
+++ server/controller/applicationController.js
@@ -71,17 +71,85 @@ exports.create = async (req, res) => {
 
 };
 
-exports.find = async (req,res) => {
-    applicationDb.find()
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ message: `cannot find applications` });
-            } else {
-                res.send(data)
-            }
-        }).catch(err => {
-            res.status(500).json({ error: 'Internal server error' + err });
-        })
-}
+exports.find = async (req, res) => {
+    try {
+        let status = req.query.status;
+        let search = req.query.search;
+        const pipeline = [];
 
+        if (status) {
+            pipeline.push({
+                $match: {
+                    status: status
+                }
+            });
+        }
+
+        if (search) {
+            pipeline.push({
+                $match: {
+                    $or: [
+                        { name: { $regex: search, $options: 'i' } },
+                        { email: { $regex: search, $options: 'i' } },
+                    ],
+                },
+            });
+        }
+
+        // If no status is provided, include a $match stage with an empty object to get all data
+        pipeline.push({
+            $match: {}
+        });
+
+        const result = await applicationDb.aggregate(pipeline).exec();
+        res.send(result);
+
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error ' + err.message });
+    }
+};
+
+
+
+// exports.update = async (req, res) => {
+//     const { id } = req.params;
+//     const { status } = req.body;
+
+//     try {
+//         const updatedApplication = await applicationDb.findByIdAndUpdate(
+//             id,
+//             { status },
+
+//         );
+
+//         if (!updatedApplication) {
+//             return res.status(404).send({ message: 'Application not found' });
+//         }
+
+//         res.send(updatedApplication);
+//     } catch (error) {
+//         res.status(500).send({ message: 'Internal server error' });
+//     }
+// };
+
+
+exports.update = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const application = await applicationDb.findById(id);
+        if (!application) {
+            return res.status(400).send({ message: `Error while updating status` });
+        }
+        // let avatarPath = application.avatars;
+        const updatedStatus = {
+            ...req.body,
+            // avatars: avatarPath
+        }
+        const updated = await applicationDb.findByIdAndUpdate(id, updatedStatus, { new: true });
+        res.status(200).json(updated);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
+}
 
--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -4,5 +4,6 @@ const applicationController = require('../controller/applicationController');
 
 applicationRoute.post('/application',applicationController.create);
 applicationRoute.get('/application',applicationController.find);
+applicationRoute.put('/application/:id',applicationController.update);
 
 module.exports = applicationRoute
